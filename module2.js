e503ecf420f8d7c3e3f47b55ea32fa47e29bd1f4
Daniel2711 <detaddeo.d@gmail.com>
2021-01-14 12:11:49 +0100

    Base

--- module2.js
+++ module2.js
@@ -1,41 +1,50 @@
+var modalita = ''
+var endGame = false;
+var sequenza = [];
+var clickUtenteInseriti = []
+var punteggio = 0
 
 $(document).ready(function () {
-startingEvent()
+
+    // ataring page
+    startingEvent('start');
+
 });
 
 
-function startingEvent(){
-    $("#modalModalita").modal({backdrop: 'static'});
+function startingEvent(info){
+    
+    preGame()  
 
-    $('#continue').click(function (e) { 
-        e.preventDefault();
-        preGame()       
-    });
 }
+
 
 async function preGame(){
 
-    // VARIABILI DI GIOCO
-    var modalita = ''
-
     // INPUT MODALITA DI GIOCO (CLASSICA, AVANZATA)
 
-    var modalita = $('#modalita .active').attr('value');
+    modalita = $('#modalita .active').attr('value');
     console.log(modalita)
 
 
     for (let index = 5; index >= 0 ; index--) {
-        console.log('attesa numero: '+ index)
+        // console.log('attesa numero: '+ index)
         await animazioneIniziale(index)        
     }
 
-    // INIZIO GIOCO
-    var endGame = false;
-    var sequenza = [1,2,3,4];
 
     console.log('sequenza: '+sequenza.toString())
 
-    while(!endGame){
+    game()
+
+
+    // RISULTATI GIOCO ( PUNTI , RECORD, LIVELLO)
+
+
+}
+
+// round function
+async function game(){
 
         // inserimento valori
         if(modalita == 'classica'){
@@ -58,28 +67,115 @@ async function preGame(){
 
         // stampa sequenza
         console.log('sequenza: '+sequenza.toString())
+        console.log('sequenzautente: '+clickUtenteInseriti.toString())
         
         // accendi la sequenza
-        await sequenzaLuci(sequenza);
+        await sequenzaLuci(sequenza)
 
-        endGame = true;
+        console.log('sequenza: '+sequenza.toString())
+        console.log('sequenzautente: '+clickUtenteInseriti.toString())
+        await attivaInput()
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+// sequenza utente
+async function clickInput(e){
+    
+    console.log('**********************')
+
+
+    numeroLampadina = parseInt(e.target.getAttribute('value'))
+    console.log('numero lampadina click: '+numeroLampadina)
+    
+    clickUtenteInseriti.push(numeroLampadina)
+    
+    console.log('click utente array: '+clickUtenteInseriti)
+    console.log('sequenza: '+sequenza)
+
+    console.log('**********************')
+
+    if(checkinputUtente()){
+
+        console.log('ancora player game'+checkinputUtente()+sequenza.length+clickUtenteInseriti.length)
+        punteggio++
+
+        if(sequenza.length == clickUtenteInseriti.length){
+            console.log('** continua a giocare **')  
+            clickUtenteInseriti = [] 
+            rimuoviInput()
+            game();
+        }
+    } else {
+        $('.center-game').text('');
+        $('#punteggio').text(punteggio);
+        $('#livello').text(sequenza.length);
+        $("#endGameWindows").modal({backdrop: 'static'});
     }
 
-    // RISULTATI GIOCO ( PUNTI , RECORD, LIVELLO)
+}
 
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Input utente
+function attivaInput(){
+
+    console.log('attivazione input')
+    $('.center-game').text('Sequenza');
+
+    $('svg path').click((e) => { 
+        // console.log(e.target)
+        e.preventDefault();
+        clickInput(e)
+    });
+
+    $('svg path').mouseenter( (e) => { 
+        // console.log('accendi lamp:' + $(e.target).attr('value') )
+        accendiLampadina($(e.target).attr('value'))
+    });
+
+    $('svg path').mouseout( (e) => { 
+        // console.log('spengo lamp: '+ $(e.target).attr('value'))
+        spegniLampadina($(e.target).attr('value'))
+    });
 
 }
+
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+// rimuovi input
+function rimuoviInput(){
+
+    console.log('rimuovi input')
+
+    $('svg path').off('click');
+
+    $('svg path').off('mouseenter');
+
+    $('svg path').off('mouseout');
+
+    $("#btnYellow").attr('style','fill: url(#gradient-3)')
+    $("#btnBlue").attr('style','fill: url(#gradient-2)')
+    $("#btnRed").attr('style','fill: url(#gradient-0)')
+    $("#btnGreen").attr('style','fill: url(#gradient-1)')
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 // sequenza luci illuminate
 async function sequenzaLuci(sequenza){
 
-    console.log('inizio luci in sequenza')
+    console.log('inizio luci in sequenza'+sequenza.toString())
 
     for ( let numero of sequenza ){
         await accendiSequenza(numero)
 
-    console.log('------------------------');
-    console.log('fine luci in sequenza');
+    // console.log('------------------------');
+    // console.log('fine luci in sequenza');
 
     }
 }
@@ -87,10 +183,10 @@ async function sequenzaLuci(sequenza){
 // accendi lampadina
 function accendiLampadina( numero ){
 
-    console.log('---------------------------')
-    console.log('Accendo lampadina: '+numero)
+    // console.log('---------------------------')
+    // console.log('Accendo lampadina: '+numero)
 
-    console.log($("#btnYellow").attr('style'))
+    // console.log($("#btnYellow").attr('style'))
 
     if(numero == 1){
         $("#btnYellow").attr('style','fill: url(#gradient-3-active)')
@@ -107,7 +203,7 @@ function accendiLampadina( numero ){
 // spegni lampadina
 function spegniLampadina(numero,resolve){
 
-    console.log('Spengo lampadina: '+numero)
+    // console.log('Spengo lampadina: '+numero)
 
     if(numero == 1){
         $("#btnYellow").attr('style','fill: url(#gradient-3)')
@@ -119,7 +215,7 @@ function spegniLampadina(numero,resolve){
         $("#btnGreen").attr('style','fill: url(#gradient-1)')
     }
 
-    setTimeout(resolve,600)
+    setTimeout(resolve,500)
 
 }
 
@@ -127,6 +223,7 @@ function spegniLampadina(numero,resolve){
  async function accendiSequenza(numero){
 
      console.log('accendi sequenza')
+     $('.center-game').text('Memorizza');
 
     return new Promise( (resolve, reject) => {
 
@@ -138,10 +235,32 @@ function spegniLampadina(numero,resolve){
     })
 
 }
+///////////////////////////////////////////////////////////////////////////////////////
+// controlla input utente
+function checkinputUtente(){
 
+    let check = true;
+    let num = clickUtenteInseriti.length-1
+
+     console.log('lungh: '+num+', sequenza: '+sequenza[num]+', click: '+clickUtenteInseriti[num])
+
+    if(sequenza[num] == clickUtenteInseriti[num]){
+        check = true;
+    } else {
+        check = false;
+    }
+
+    console.log('** '+check+' **')
+    return check
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////// 
+
+// animazione partita
 function animazioneIniziale(num){
 
-    console.log('animazione iniziale: '+num)
+    // console.log('animazione iniziale: '+num)
 
     if(num == 5){
        console.log('wait 1 s')
